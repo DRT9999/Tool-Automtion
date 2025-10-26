59b9c242a86a0b1b91ea69dc396443d2e9d475d9
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-26 17:59:35 +0530

    vault

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "tool" {
             instance_type = "t2.micro"
             port_no =8200
         }
-        vault  ={
+        prometheus  ={
             name = "prometheus"
             instance_type = "t2.micro"
             port_no =9090 
