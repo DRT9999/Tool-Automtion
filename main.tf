682ddcec83d6dfa4601adce6bfc7b944324fbe90
DRT9999 <ravitejadevinidi@gmail.com>
2025-10-26 18:37:45 +0530

    vault

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "Tool" {
     for_each = var.tool
     source = "./module"
     
-    name = var.tool["vault"]["name"]
-    instance_type = var.tool["vault"]["instance_type"]
-    port_no = var.tool["vault"]["port_no"]
+    name = each.value["name"]
+    instance_type = each.value ["instance_type"]
+    port_no = each.value ["port_no"]
 }
